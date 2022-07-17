fbfcd79b524aadd6c0e5e12db822e8c5d81211c5
Harshit Dhyani <harshitdhyani575@gmail.com>
2022-07-17 13:18:27 +0530

    Create 1769-Minimum Number of Operations to Move All Balls to Each Box.java

---
+++ src/Codeforces/LeetCode/1769-Minimum Number of Operations to Move All Balls to Each Box.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        
+        int n = boxes.length();
+        int[] ans = new int[n];
+        
+         int[] left = new int[n];
+        
+         int[] right = new int[n];
+        
+        int cnt = boxes.charAt(0)-'0';
+        
+        for(int i=1;i<n;i++){
+            left[i] = left[i-1]+ cnt;
+            cnt+=boxes.charAt(i)-'0';
+        }
+        
+          cnt = boxes.charAt(n-1)-'0';
+        
+        for(int i=n-2;i>=0;i--){
+            right[i] = right[i+1]+ cnt;
+            cnt+=boxes.charAt(i)-'0';
+        }
+
+        for(int i=0;i<n;i++){
+            ans[i] = left[i]+ right[i];
+        }
+        return ans;
+        
+        
+        
+    }
+}
